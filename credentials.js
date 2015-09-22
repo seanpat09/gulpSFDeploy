9a478bfe013784d673e23d44ba87f3434fa84416
Sean Cuevo <seanpat09@gmail.com>
2015-09-22 17:44:19 -0600

    initial draft

---
+++ credentials.js
@@ -0,0 +1,8 @@
+'use strict'
+var username = YOUR_USERNAME_HERE_IN_QUOTES;
+var password = YOUR_PASS_AND_SECURITY_TOKEN_IN_QUOTES;
+
+module.exports = {
+    username : username,
+    password : password
+};
---
+++ metadataList.js
@@ -0,0 +1,23 @@
+//List all classes you want to deploy here
+var classes = [
+// 'ContactFactory.cls',
+// 'SObjectFactory.cls'
+];
+
+
+//List all triggers you want to deploy here
+var triggers = [
+// 'User.trigger'
+];
+
+
+//List all triggers you want to deploy here
+var staticresources = [
+// 'resourceToCompile.resource'
+];
+
+module.exports = {
+    classes: classes,
+    triggers : triggers,
+    staticresources : staticresources
+};
